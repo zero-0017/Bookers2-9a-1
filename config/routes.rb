cf0bd313fa91e939cd4778dfdc160b092136d4f8
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-03 13:48:42 +0000

    [応用課題4]フォロー・フォロワー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   resources :book_comments, only: [:create, :destroy]
 end
 
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+      resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
